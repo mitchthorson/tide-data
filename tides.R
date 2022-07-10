8f2857ab4f78d4a4febc980c9982d64b1f35c5ef
mitchthorson <mitchell@mitchellthorson.com>
2022-07-10 00:24:30 -0400

    cleanup

--- tides.R
+++ tides.R
@@ -2,7 +2,8 @@ library("tidyverse")
 library("httr")
 
 # function that takes a station_id string and a begin_date string in format of 20220708
-# returns TK
+# saves data to csv file, prints a message, and returns nothing.
+# couldn't quite parse response to data frame directly, could be nice later.
 get_station_data <- function(station_id, begin_date) {
   data_url <- sprintf("https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?product=water_level&application=NOS.COOPS.TAC.WL&begin_date=%s&end_date=20220709&datum=MLLW&station=%s&time_zone=GMT&units=english&format=csv", begin_date, station_id)
   resp <- httr::GET(data_url)
@@ -11,24 +12,20 @@ get_station_data <- function(station_id, begin_date) {
   writeBin(c, sprintf("output_data/%s_%s.csv", station_id, begin_date))
   print(sprintf("Saved data for: %s_%s", station_id, begin_date))
 }
-# sample tide data url: "https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?product=water_level&application=NOS.COOPS.TAC.WL&begin_date=20220708&end_date=20220709&datum=MLLW&station=8413320&time_zone=GMT&units=english&format=csv"
 
 # data starting from:
 begin_date <- "20220708"
 
-# data from station id:
-
-# bar harbor
+# Bar Harbor, Maine
 # station_id <- "8413320"
 
-# The Battery New York, NY
+# The Battery New York, New York
 station_id <- "8518750"
 
+# data from https://tidesandcurrents.noaa.gov/waterlevels.html?id=8518750
+# https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?product=water_level&application=NOS.COOPS.TAC.WL&begin_date=20220708&end_date=20220709&datum=MLLW&station=8518750&time_zone=GMT&units=english&format=csv
 get_station_data(station_id, begin_date)
 
-# data from https://tidesandcurrents.noaa.gov/waterlevels.html?id=8518750
-
-# https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?product=water_level&application=NOS.COOPS.TAC.WL&begin_date=20220708&end_date=20220709&datum=MLLW&station=8518750&time_zone=GMT&units=english&format=csv
 nyc_water_levels_raw <- read_csv(sprintf("./output_data/%s_%s.csv", station_id, begin_date))
 
 nyc_water_levels <- nyc_water_levels_raw %>%
